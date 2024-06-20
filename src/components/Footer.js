eafde314412775fe8b60454364c51e8cffed46ca
sidhant pandey <sidhantpandeynp6@gmail.com>
2024-06-20 22:12:13 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='flex justify-center' style={{backgroundColor: '#232528', color: 'aquamarine'}}>
+            <span>© 2024, Made with ❤️ by <a href="https://initializ.ai" >initializ inc</a> </span>
+        </div>
+    )
+}
+
+export default Footer
